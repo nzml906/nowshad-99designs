3cfedcfd6c9c6d1526deced282eccac2b81bd856
Nazmul <grey000906@gmail.com>
2020-05-29 11:42:13 +0600

    deploy portfolio

---
+++ precache-manifest.98fe325ffaa81b059ad97daf2e488239.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ead17133326c13a17fd",
+    "url": "/nowshad-99designs/css/app.0ad0b803.css"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/nowshad-99designs/favicon.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nowshad-99designs/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nowshad-99designs/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nowshad-99designs/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nowshad-99designs/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/nowshad-99designs/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/nowshad-99designs/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/nowshad-99designs/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/nowshad-99designs/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/nowshad-99designs/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/nowshad-99designs/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/nowshad-99designs/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/nowshad-99designs/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/nowshad-99designs/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/nowshad-99designs/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/nowshad-99designs/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/nowshad-99designs/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/nowshad-99designs/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/nowshad-99designs/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/nowshad-99designs/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/nowshad-99designs/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/nowshad-99designs/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/nowshad-99designs/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/nowshad-99designs/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "cf063d0b926c00b34ae139359639226f",
+    "url": "/nowshad-99designs/img/brand/99.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/nowshad-99designs/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/nowshad-99designs/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/nowshad-99designs/img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/nowshad-99designs/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "de09a3845662eb36f0351b64d0fb873a",
+    "url": "/nowshad-99designs/img/brand/nowblue.png"
+  },
+  {
+    "revision": "5a152c12f6a5e6380af42fe5e7acd9c1",
+    "url": "/nowshad-99designs/img/brand/nowico.png"
+  },
+  {
+    "revision": "e323aecf28229b9785925a5b08a1aba8",
+    "url": "/nowshad-99designs/img/brand/nowt.png"
+  },
+  {
+    "revision": "dddf3f3d388194f5bf4fe6599b87caed",
+    "url": "/nowshad-99designs/img/brand/nz.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/nowshad-99designs/img/brand/white.png"
+  },
+  {
+    "revision": "86749ea308ac63c611d7f1a48e59dd42",
+    "url": "/nowshad-99designs/img/c1.png"
+  },
+  {
+    "revision": "dcbfbbc5cd8deed160a1dd691a51c4ab",
+    "url": "/nowshad-99designs/img/c2.png"
+  },
+  {
+    "revision": "9a0bb4a4e919b4d508a1925cd7f66b02",
+    "url": "/nowshad-99designs/img/c3.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nowshad-99designs/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "002e9c823b9793c09f053eeabb338ed0",
+    "url": "/nowshad-99designs/img/i1.png"
+  },
+  {
+    "revision": "cf4eb0c186ee545fc20876040af8bfeb",
+    "url": "/nowshad-99designs/img/i2.png"
+  },
+  {
+    "revision": "33407e210a1b29903348df3675c9ea27",
+    "url": "/nowshad-99designs/img/i3.png"
+  },
+  {
+    "revision": "88cc7a179722d4f40d7a865d6929db35",
+    "url": "/nowshad-99designs/img/i4.png"
+  },
+  {
+    "revision": "b5bf276909fd3f5c8b82f57fdf01f60f",
+    "url": "/nowshad-99designs/img/i5.png"
+  },
+  {
+    "revision": "f9a54088d7a7ad42e622779cd218fa97",
+    "url": "/nowshad-99designs/img/i6.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/nowshad-99designs/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "9689a5655f031298264dac83ebb21535",
+    "url": "/nowshad-99designs/img/ipro.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/nowshad-99designs/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/nowshad-99designs/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/nowshad-99designs/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/nowshad-99designs/img/theme/landing.jpg"
+  },
+  {
+    "revision": "1a93f9369f04c67932dd2d6f09ad32ec",
+    "url": "/nowshad-99designs/img/theme/portfolio.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/nowshad-99designs/img/theme/profile.jpg"
+  },
+  {
+    "revision": "6bc59e8ecb9004b144c97dec2d1710a9",
+    "url": "/nowshad-99designs/img/theme/profile.png"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/nowshad-99designs/img/theme/promo-1.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/nowshad-99designs/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/nowshad-99designs/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/nowshad-99designs/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/nowshad-99designs/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "af3045559192ab9aefb335f414d7b2de",
+    "url": "/nowshad-99designs/index.html"
+  },
+  {
+    "revision": "6ead17133326c13a17fd",
+    "url": "/nowshad-99designs/js/app.02f0a8db.js"
+  },
+  {
+    "revision": "f7fc1446ce97b9150565",
+    "url": "/nowshad-99designs/js/chunk-vendors.6849c9e0.js"
+  },
+  {
+    "revision": "c927ef8e1ec933f54cde7907dff241f9",
+    "url": "/nowshad-99designs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nowshad-99designs/robots.txt"
+  }
+]);
